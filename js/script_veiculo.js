5c5bc2d034f35ebb083868db7b67d72a884cbb6b
vivian suenny <vivian.cruz@aluno.senai.br>
2026-06-22 21:22:38 -0300

    atividade 03

---
+++ js/script_veiculo.js
@@ -0,0 +1,19 @@
+import {calcularIPVA}
+from"./script_calculo.js"
+let formulario= document.getElementById("formveiculo");
+let lista= document.getElementById("listaveiculos");
+formulario.addEventListener("submit",function(event){
+event.preventDefault()
+let marca
+})
+
+
+
+
+const formModelo = document.querySelector('#modelo')
+const formMarca = document.querySelector('#marca')
+const formPlaca = document.querySelector('#placa')
+const formFabricado = document.querySelector('#fabricado')
+const formValor = document.querySelector('#valor')
+const formCombustivel = document.querySelector('#combustivel')
+
